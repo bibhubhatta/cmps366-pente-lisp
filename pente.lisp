24f9adf8bc5f8b6e99c486dc8fae19cd61e9fba6
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-10-04 00:41:52 -0400

    Add function cartesian_board
    Adds row and column markers to the board
    Takes the unmarked board as a parameter
    Returns board with position markers

--- pente.lisp
+++ pente.lisp
@@ -168,6 +168,114 @@
 )
 
 
+;;; *********************************************
+;;; Name   : mark_rows
+;;; Purpose: Mark the rows of the board
+;;; Return : The board -- a list of lists
+;;; *********************************************
+(defun mark_rows (unmarked_board)
+
+  (cond 
+        ((null unmarked_board) nil)
+        (t (cons 
+                    ; Mark the first element of the list with the length of the list
+                    (cons 
+                           (length unmarked_board)
+                           (car unmarked_board)
+                    ) 
+                    ; Recursively mark the rest of the list and cons the result
+                    (mark_rows (cdr unmarked_board))
+           )
+        )
+        
+  )
+
+)
+
+;;; *********************************************
+;;; Name   : range
+;;; Purpose: Get a list of numbers from start 
+;;;          to end (start inclusive, end exclusive)
+;;; Return : The list of numbers
+;;; *********************************************
+
+(defun range (start end)
+  (cond
+        ; Since the range is exclusive of the end,
+        ; we have to check if the start + 1 is greater
+        ; than or equal to the end
+        ((>= (+ 1 start) end) nil)
+        (t (cons start (range (+ 1 start) end)))
+  )
+)
+
+
+;;; *********************************************
+;;; Name   : num_list_to_char_list
+;;; Purpose: Convert a list of numbers to a list
+;;;          of characters
+;;; Return : The list of characters
+;;; *********************************************
+(defun num_list_to_char_list (num_list)
+
+  (cond
+        ((null num_list) nil)
+        (t (cons (code-char (+ 64 (car num_list))) (num_list_to_char_list (cdr num_list))))
+  )
+
+)
+
+
+;;; *********************************************
+;;; Name   : mark_columns
+;;; Purpose: Mark the columns of the board
+;;; Return : The board -- a list of lists
+;;; *********************************************
+(defun mark_columns (unmarked_board)
+  
+    (append
+            
+             unmarked_board
+             
+             
+             (list (num_list_to_char_list (range 0 (+ 1 (length (car unmarked_board))))))
+            
+
+    )
+)
+
+
+;;; *********************************************
+;;; Name   : cartesian_board
+;;; Purpose: Get board with position indicators
+;;;          for each row and column
+;;; Return : The board -- a list of lists
+;;; *********************************************
+(defun cartesian_board (unmarked_board)
+
+  (mark_columns
+                (
+                  mark_rows unmarked_board
+                )
+  )
+
+)
+
+
 ;;;; ******************************************************************
 ;;;; End of board related functions
 ;;;; ******************************************************************
+
+
+(
+  print
+        (
+          cartesian_board
+            (
+              board
+                (
+                  case_4
+                )
+            )
+        )
+)
