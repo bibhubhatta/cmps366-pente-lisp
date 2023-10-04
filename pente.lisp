d0c8ae734147f9170fadafcc5f5dfa09958f58fb
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2023-10-03 23:08:18 -0400

    Copy Serialization list provided by Prof 0.5
    The game states will be used for debugging functions
    They are returned by a function so that the game state
    can be passed into other functions

---
+++ pente.lisp
@@ -0,0 +1,153 @@
+;;;; ******************************************************************
+;;;; Serialization lists
+;;;; Used for debugging and final demo
+;;;; ******************************************************************
+
+(defun case_1 ()
+  '(
+      ; Board:
+      (
+      ( O O O O O O O O O O O O B O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O W O O O O O O O O O O O O O O O O )
+      ( O O W O O O O O O O O O O O O O O O O )
+      ( O O B O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O W W B O O O O O O )
+      ( O O O O O O O O O W O O O O O O O O O )
+      ( O O O O O O O O O W O O O O O O O O O )
+      ( O O O O O O O B O B O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O B O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O B O O O O O O O O O O O O O O O O )
+      ( O O W O O O O O O O O O O O O O O O O )
+      ( O O W O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      )
+
+      ; Human
+      0 0
+
+      ; Computer
+      0 0
+
+      ; Next player
+      Human Black
+  ) 
+)
+
+
+(defun case_2 ()
+  '(
+      ; Board:
+      (
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O W W B O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O B O O O O )
+      ( O O O O O B O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O B O O O O O O O )
+      ( O O O O O O O O O O W O O O O O O O O )
+      ( O O O O O O O O O W O O O O O O O O O )
+      ( O O O O O O W W O O O O O O O O O O O )
+      ( O O O O O O O O O W O O O O O O O O O )
+      ( O O O O O O O O O O W O O O O O O O O )
+      ( O O B O O O O O O O O B O O O O O O O )
+      ( O O O O O B O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O B O O O )
+      ( O O O W W B O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      )
+
+      ; Human
+      0 0
+
+      ; Computer
+      0 0
+
+      ; Next player
+      Computer Black   
+  )
+)
+
+( defun case_3 ()
+  '(
+    ; Board:
+    (
+      ( O O O W W B O O O O O O O O O O O O O )
+      ( O O W O O B O O O O O O O B B O O O O )
+      ( O O W O O O O O O O O O O O O O O O O )
+      ( O O B O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O B O O O O O )
+      ( O O O W O O O O O O O O O O O O O O O )
+      ( O O O O W O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O W O O O O O O O O O O O O )
+      ( O O O O O O O W O W O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O B B O O B B O O O O O O O O )
+      ( O O O O O W O O O O O O O O O O O O O )
+      ( O O O O O W O O O O O O O O B O O O O )
+      ( O O B W W O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+    )
+
+    ; Human
+    1 0
+
+    ; Computer
+    0 0
+
+    ; Next player
+    Human Black
+  )
+)
+
+(defun case_4 ()
+  '(
+    ; Board:
+    (
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O B B O O O O O O O O O O O O O O )
+      ( O O O B O B O O O O O O O O O O O O O )
+      ( O O O W O O W O O O O O O O O O O O O )
+      ( O O O O O B O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O W O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O W O O O O O O O O O )
+      ( O O O O O O O O W O O O O O O O O O O )
+      ( O O O O O O O W O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O O O O O )
+      ( O O O O O O O O O O O O O O O B O O O )
+      ( O O O O O O O O O O O O O O O B O O O )
+      ( O O O O W W B O O O O O O O O O O O O )
+      ( O O O W O O O O O O O O O O O O O O O )
+      ( O O O W O O O O O O O O O O O O O O O )
+      ( O O O B O O O O O O O O O O O O O O O )
+    )
+
+    ; Human
+    3 6
+
+    ; Computer
+    0 2
+
+    ; Next player
+    Computer White
+  )
+)
+
+;;;; ******************************************************************
+;;;; End of serialization lists
+;;;; ******************************************************************
